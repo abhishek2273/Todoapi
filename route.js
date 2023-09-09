81b720377f37e7a9224b1654b40b9d8bc62ddd70
Abhishek Sen <105163544+abhishek2273@users.noreply.github.com>
2023-09-09 15:52:34 +0530

    Update route.js
    withCredentials

--- route.js
+++ route.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    withCredentials: true,
 }))
 
 //Routes
